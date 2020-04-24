509528ae86e72ba7ace1d7341b8cd6407a8a7a8b
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2020-04-24 17:09:35 +0400

    fix: build dev

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,4 +1,5 @@
 export default [
+  require("/Users/giga/projects/vobees-doc/node_modules/@docusaurus/plugin-google-gtag/src/gtag"),
   require("infima/dist/css/default/default.css"),
   require("remark-admonitions/styles/infima.css"),
   require("/Users/giga/projects/vobees-doc/src/css/custom.css"),
